a438c6bef16d595c5347aa96e83e0a789567f438
Giacomo Roggero <groggero@student.ethz.ch>
2026-04-27 18:07:13 +0200

    firstTry

--- mhealth_activity/__init__.py
+++ mhealth_activity/__init__.py
@@ -1,3 +1,11 @@
 from .recording import Recording
 from .trace import Trace
 from .types import Activity, Path, WatchLocation
+from .activity_recognition import (
+    ACTIVITY_ORDER as ACTIVITY_RECOGNITION_ORDER,
+    ActivityRecognizer,
+    activity_f1_summary,
+    build_activity_recognizer,
+    evaluate_activity_recognizer,
+    extract_window_feature_rows as extract_activity_window_feature_rows,
+)
